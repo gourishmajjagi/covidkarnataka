75eda36a1767998c3dc102b37490a3b040d6f12c
Aishwarya Majjagi <amajjagi1997@gmail.com>
2020-07-18 10:45:59 -0700

    hicharts initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { DashboardModule } from './dashboard/dashboard.module';
+import { HighchartsChartModule } from 'highcharts-angular';
+
 import { AppComponent } from './app.component';
+import { ChartDonutComponent } from './shared/chart-donut/chart-donut.component';
+import { ChartLineComponent } from './shared/chart-line/chart-line.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartDonutComponent,
+    ChartLineComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HighchartsChartModule,
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/chart-donut/chart-donut.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-donut',
+  templateUrl: './chart-donut.component.html',
+  styleUrls: ['./chart-donut.component.css']
+})
+export class ChartDonutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
